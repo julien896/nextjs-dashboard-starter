d8bf517c76f766e43d1ac7a66e1d84a3f49f9226
Julian <julicogui@gmail.com>
2024-01-18 10:48:56 -0300

    feat: content components structured

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,6 +1,26 @@
+import Card from "./_components/card/card"
+import Chart from "./_components/chart/chart"
+import RightBar from "./_components/rightbar/right-bar"
+import Transactions from "./_components/transactions/transactions"
+
+import styles from './dashboard.module.css'
+
 const DashboardPage = () => {
   return (
-    <div>DashboardPage</div>
+    <div className={styles.wrapper}>
+      <div className={styles.main}>
+        <div className={styles.cards}>
+          <Card />
+          <Card />
+          <Card />
+        </div>
+        <Transactions />
+        <Chart />
+      </div>
+      <div className={styles.side}>
+        <RightBar />
+      </div>
+    </div>
   )
 }
 
